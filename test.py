9e41acc3aeda7b7b1a0a6f341850482fb7a71458
Kevin Thomas <kevin@mytechnotalent.com>
2022-04-14 13:53:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-   def draw_image(self, path, x=0, y=0, w=240, h=320, draw_speed=26375):
+    def block(self, x0, y0, x1, y1, data):
+        """
+        Method to write a block of data to display.
+
+        Params:
+            x0: int
+            y0: int
+            x1: int
+            y1: int
+            data: bytes
+        """
+        self.write_cmd(self.SET_COLUMN, *ustruct.pack('>HH', x0, x1))
+        self.write_cmd(self.SET_PAGE, *ustruct.pack('>HH', y0, y1))
+        self.write_cmd(self.WRITE_RAM)
+        self.write_data(data)
+         
+         
+    def draw_image(self, path, x=0, y=0, w=240, h=320):
         """
         Method to draw image on screen from flash or sd card
 
@@ -15,40 +32,13 @@
         y2 = y + h - 1
         if self.is_off_grid(x, y, x2, y2):
             return
+        buf = bytearray(240*320*2)  # 153600 total byte array of an image
         with open(path, 'rb') as f:
-            chunk_height = draw_speed // w  # 153600 total bytes of an image
-            #chunk_count, remainder = divmod(h, chunk_height)
-            chunk_size = chunk_height * w * 2
-            chunk_y = y
-            #buf = bytes(0)
-      #      if chunk_count:
-   #             for _ in range(0, chunk_count):
-   #                 gc.collect()
-            buf = f.read(chunk_size)
-                    #self.block(x, chunk_y, x2, chunk_y + chunk_height - 1, buf)
-   #                 chunk_y += chunk_height
-            #if remainder:
-             #   gc.collect()
-            #    buf += f.read(remainder * w * 2)
-                #self.block(x, chunk_y, x2, chunk_y + remainder - 1, buf)
-            buf = framebuf.FrameBuffer(bytearray(buf), w, h, framebuf.RGB565)
-            self.block(x, chunk_y, x2, chunk_y + chunk_height - 1, buf)
-
-            
-            
-            
-            
- 
-def write_bin(f, pixel_list):
-    """
-    Method to save image in RGB565 format
-    
-    Params:
-        f: object
-        pixel_list: list
-    """
-    for pix in pixel_list:
-        r = (pix[0] >> 3) & 0x1F
-        g = (pix[1] >> 2) & 0x3F
-        b = (pix[2] >> 3) & 0x1F
-        f.write(pack('>H', (r << 11) + (g << 5) + b))
+            bytes_read = f.readinto(buf)
+            #gc.collect()
+        fb = framebuf.FrameBuffer(buf, 240, 320, framebuf.RGB565)
+        self.block(x, y, x2, y2, fb)
+   
+   
+0 File "ili9341.py", line 356, in draw_image
+MemoryError: memory allocation failed, allocating 153600 bytes
