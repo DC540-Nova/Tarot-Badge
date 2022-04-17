eba5d81025c4b3894c9a2cfe1c7407a5ac66c2cc
Kevin Thomas <kevin@mytechnotalent.com>
2022-04-17 18:13:12 -0400

    Added instructions

---
+++ copy-files-to-pico.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mpremote connect /dev/tty.u* cp Unispace12x24.c :
+mpremote connect /dev/tty.u* cp config.py :
+mpremote connect /dev/tty.u* cp dc540_logo.raw :
+mpremote connect /dev/tty.u* cp demo.py :
+mpremote connect /dev/tty.u* cp ili9341.py :
+mpremote connect /dev/tty.u* cp main.py :
+mpremote connect /dev/tty.u* cp neo_pixel.py :
+mpremote connect /dev/tty.u* cp sdcard.py :
+mpremote connect /dev/tty.u* cp xglcd_font.py :
+
