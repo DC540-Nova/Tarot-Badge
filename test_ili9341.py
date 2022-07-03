15281907d2559cec9c1c0b7f377516e8469675f9
Kevin Thomas <kevin@mytechnotalent.com>
2022-07-03 19:16:41 -0400

    Update

--- test_ili9341.py
+++ test_ili9341.py
@@ -36,7 +36,7 @@
 import unittest
 from machine import Pin, SPI
 
-from ili9341 import Display
+from ili9341 import XglcdFont, Display
 
 
 class TestDisplay(unittest.TestCase):
@@ -49,6 +49,7 @@ class TestDisplay(unittest.TestCase):
         """
         display_spi = SPI(0, baudrate=40000000, sck=Pin(6, Pin.OUT), mosi=Pin(7, Pin.OUT))
         self.display = Display(display_spi, dc=Pin(15, Pin.OUT), cs=Pin(13, Pin.OUT), rst=Pin(14, Pin.OUT))
+        self.UNISPACE_FONT = XglcdFont('Unispace12x24.c', 12, 24)  # load font
 
     def tearDown(self):
         """
@@ -64,45 +65,79 @@ class TestDisplay(unittest.TestCase):
         got = self.display.__read_reg(0x01)  # noqa
         self.assertEqual(got, want)
 
-    # def test_decryption_small_length_word(self):
-    #     """
-    #     test decryption small length word functionality
-    #     """
-    #     want = 0xd, 0x10, 0xd,
-    #     got = self.encryption.cipher('d', want, 10)
-    #     self.assertEqual(got, 'f')
-    #
-    # def test_decryption_medium_length_word(self):
-    #     """
-    #     test decryption medium length word functionality
-    #     """
-    #     want = 0x71, 0x10, 0x19, 0x71,
-    #     got = self.encryption.cipher('d', want, 10)
-    #     self.assertEqual(got, 'fo')
-    #
-    # def test_decryption_large_length_word(self):
-    #     """
-    #     test decryption large length word functionality
-    #     """
-    #     want = 0x9, 0x10, 0x19, 0x19, 0x9,
-    #     got = self.encryption.cipher('d', want, 10)
-    #     self.assertEqual(got, 'foo')
-    #
-    # def test_decryption_extra_large_length_word(self):
-    #     """
-    #     test decryption extra large length word functionality
-    #     """
-    #     want = 0x10, 0x19, 0x15, 0x19, 0x4a, 0x12, 0xc, 0xb, 0x73, 0x1c, 0x4a, 0xe, 0x10, 0xf, 0x6b, 0x4a, 0x10, 0x68, 0x19, 0x4a, 0x1a, 0x10, 0x1f, 0x77, 0x17, 0x4a, 0x79, 0xb, 0x18, 0xb, 0xe, 0x4a, 0x65, 0xb, 0x4a, 0x17, 0xc, 0x19, 0x62, 0x1e, 0x1e, 0x70, 0x16, 0xf, 0xc, 0x4a, 0x19, 0x10,  # noqa
-    #     got = self.encryption.cipher('d', want, 10)
-    #     self.assertEqual(got, 'foo bar fe fo fum and a bottle of')
-    #
-    # def test_decryption_over_length_word(self):
-    #     """
-    #     test decryption over length word functionality
-    #     """
-    #     want = 'message must be less than 34 chars'
-    #     got = self.encryption.cipher('d', want, 10)
-    #     self.assertEqual(got, want)
+    def test_write_reg(self):
+        """
+        test write_reg functionality
+        """
+        want = None
+        got = self.display.__write_reg(self.display.RAMWR)  # noqa
+        self.assertEqual(got, want)
+
+    def test_write_data(self):
+        """
+        test write_data functionality
+        """
+        want = None
+        got = self.display.__write_data('foo')  # noqa
+        self.assertEqual(got, want)
+
+    def test_block(self):
+        """
+        test block functionality
+        """
+        want = None
+        got = self.display.__block(1, 2, 3, 4, 'foo')  # noqa
+        self.assertEqual(got, want)
+
+    def test_letter(self):
+        """
+        test letter functionality
+        """
+        want_width = 11
+        want_height = 24
+        got_width, got_height = self.display.__letter('a', 0b1111111111100000, self.UNISPACE_FONT, 1, 2, 0)  # noqa
+        self.assertEqual(got_width, want_width)
+        self.assertEqual(got_height, want_height)
+
+    def test_clear(self):
+        """
+        test clear functionality
+        """
+        want = None
+        got = self.display.clear()
+        self.assertEqual(got, want)
+
+    def test_text(self):
+        """
+        test text functionality
+        """
+        want = None
+        got = self.display.text('foo')
+        self.assertEqual(got, want)
+
+    def test_image(self):
+        """
+        test image functionality
+        """
+        want = None
+        got = self.display.image('dc540_logo.raw')
+        self.assertEqual(got, want)
+
+    def test_handle_threading_setup(self):
+        """
+        test handle_threading_setup functionality
+        """
+        want = None
+        got = self.display.handle_threading_setup()
+        self.assertEqual(got, want)
+
+    def test_handle_threading_teardown(self):
+        """
+        test handle_threading_teardown functionality
+        """
+        want = None
+        got = self.display.handle_threading_teardown()
+        self.assertEqual(got, want)
 
 
 if __name__ == '__main__':
