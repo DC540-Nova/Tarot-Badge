9863c1c8dedfd70191259364a991d709a7a18d0c
Kevin Thomas <kevin@mytechnotalent.com>
2022-07-13 13:30:16 -0400

    Update

--- copy_files_to_sd_card.sh
+++ copy_files_to_sd_card.sh
@@ -3,7 +3,7 @@
 source venv/bin/activate
 if [[ $2 == 1 || $2 == 2 || $2 == 3 || $2 == 4 ]]
 then
-    mpremote connect /dev/tty.u* mkdir /sd/$2 || true
+    mpremote connect /dev/tty.u* mkdir /sd/$2 >/dev/null
     mpremote connect /dev/tty.u* cp $1*.* :/sd/$2/
 else
     echo "  usage: ./copy_files_to_sd_card.sh <directory> <1, 2, 3 or 4>"
