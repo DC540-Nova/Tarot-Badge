7209d2803557db72ad7342b506458dc5af6e8835
Kevin Thomas <kevin@mytechnotalent.com>
2022-07-14 10:23:54 -0400

    Update

--- copy_files_to_sd_card.sh
+++ copy_files_to_sd_card.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 source venv/bin/activate
-if [[ $2 == 1 || $2 == 2 || $2 == 3 || $2 == 4 ]]
+if [[ ($2 =~ [^A-Za-z0-9]) || (-z $2) || ! (-z $3) || (${#2} -gt 8) ]]
 then
-    mpremote connect /dev/tty.u* mkdir /sd/$2 >/dev/null
-    mpremote connect /dev/tty.u* cp $1*.* :/sd/$2/
+  echo "  usage: ./copy_files_to_sd_card.sh <directory> <card_deck>"
 else
-    echo "  usage: ./copy_files_to_sd_card.sh <directory> <1, 2, 3 or 4>"
+    echo "That URL is allowed."
 fi
+#    mpremote connect /dev/tty.u* mkdir /sd/$2 >/dev/null
+#    mpremote connect /dev/tty.u* cp $1*.* :/sd/$2/
