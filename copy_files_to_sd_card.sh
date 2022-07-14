8ae9e28b8b67b28a9d86249f5be8fa5390e4d843
Kevin Thomas <kevin@mytechnotalent.com>
2022-07-14 10:26:56 -0400

    Update

--- copy_files_to_sd_card.sh
+++ copy_files_to_sd_card.sh
@@ -5,7 +5,6 @@ if [[ ($2 =~ [^A-Za-z0-9]) || (-z $2) || ! (-z $3) || (${#2} -gt 8) ]]
 then
   echo "  usage: ./copy_files_to_sd_card.sh <directory> <card_deck>"
 else
-    echo "That URL is allowed."
+    mpremote connect /dev/tty.u* mkdir /sd/"$2" >/dev/null
+    mpremote connect /dev/tty.u* cp "$1"*.* :/sd/"$2"/
 fi
-#    mpremote connect /dev/tty.u* mkdir /sd/$2 >/dev/null
-#    mpremote connect /dev/tty.u* cp $1*.* :/sd/$2/
